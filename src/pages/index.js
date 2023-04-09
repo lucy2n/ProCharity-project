624e28eb4e41555eea8ccb528555ee1bd10fe94e
lucy2n <lyusianaumenko@yandex.by>
2023-04-09 17:25:22 +0300

    feat: настройка cropperjs

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,3 +39,19 @@ function renderSuggestionsList() {
   tagify.dropdown.show.call(tagify) // load the list
   tagify.DOM.scope.parentNode.appendChild(tagify.DOM.dropdown)
 }
+
+/* Cropper */
+
+import Cropper from 'cropperjs';
+
+const image = document.getElementById('popup__avatar');
+const cropper = new Cropper(image, {
+  aspectRatio: 1 / 1
+});
+
+cropper.options.highlight = false;
+cropper.options.dragMode = "move";
+cropper.options.guides = false;
+cropper.options.center = false;
+cropper.options.cropBoxMovable = false;
+cropper.options.background = false;
