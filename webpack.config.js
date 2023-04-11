9e5b8bf0cbfd50a6c5fa9d880aaf12fd44548220
Falor <max89.bobyr@yandex.ru>
2023-04-11 17:35:22 +0300

    feat: добавил UiKit

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/UIKit.html'
+      template: 'src/notifications.html'
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
