99549b47c3f58ada1b78ac7ad740ab3d37cbd119
Kristina <tinakyukova@gmail.com>
2023-04-11 18:17:32 +0300

    feat: обозначина структура страницы

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/UIKit.html'
+      template: 'src/v_account_motivation.html'
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
