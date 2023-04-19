6505ddaec7c508bdc708519425b10147ec7124ab
lucy2n <lyusianaumenko@yandex.by>
2023-04-19 13:53:52 +0300

    feat: добавить открытие и закрытие попапа

--- src/pages/contacts/index.js
+++ src/pages/contacts/index.js
@@ -1,6 +1,18 @@
 import './index.css';
 import Cropper from 'cropperjs';
 
+const popup = document.querySelector(".popup");
+const avatarButton = document.querySelector(".avatar-box__link")
+const closeButton = document.querySelector(".popup__button-close")
+
+avatarButton.addEventListener('click', () => {
+    popup.classList.add('popup_opened');
+});
+
+closeButton.addEventListener('click', () => {
+    popup.classList.remove('popup_opened')
+});
+
 const image = document.getElementById('popup__avatar');
 const cropper = new Cropper(image, {
     aspectRatio: 1/1,
@@ -13,3 +25,5 @@ cropper.options.guides = false;
 cropper.options.center = false;
 cropper.options.cropBoxMovable = false;
 cropper.options.background = false;
+cropper.options.minContainerWidth = 472;
+cropper.options.minContainerHeight = 298;
