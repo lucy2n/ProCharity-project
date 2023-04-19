d773b15dc1eee92b3df5dc3435838fa02f3d6c4a
lucy2n <lyusianaumenko@yandex.by>
2023-04-19 17:58:34 +0300

    feat: добавлен js для dropdown input

--- src/pages/contacts/index.js
+++ src/pages/contacts/index.js
@@ -1,6 +1,8 @@
 import './index.css';
 import Cropper from 'cropperjs';
 
+/* Popup open/close */
+
 const popup = document.querySelector(".popup");
 const avatarButton = document.querySelector(".avatar-box__link")
 const closeButton = document.querySelector(".popup__button-close")
@@ -12,6 +14,45 @@ avatarButton.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
     popup.classList.remove('popup_opened')
 });
+
+/* Dropdown */
+
+function handleSelect(input) {
+    const dropdown = input.closest(".input-case__dropdown");
+    const dropdownContent = dropdown.querySelector(".input-case__dropdown-content");
+    input.addEventListener('click', () => {
+        dropdownContent.classList.add('input-case__dropdown-content_active');
+    })
+    const options = dropdownContent.querySelectorAll(".input-case__option");
+
+    options.forEach((option) => {
+        option.addEventListener('click', () => {
+            clearOptions(options)
+
+            const optionImage = option.querySelector(".input-case__image");
+            optionImage.classList.add("input-case__image_active");
+            const optionTitle = option.querySelector(".option__title");
+            input.value = optionTitle.textContent;
+
+            dropdownContent.classList.remove("input-case__dropdown-content_active");
+        })
+    })
+}
+
+function clearOptions(options) {
+    options.forEach((option) => {
+        const optionImage = option.querySelector(".input-case__image");
+        optionImage.classList.remove("input-case__image_active");
+    });
+}
+
+const firstInput = document.querySelector("#first_input");
+handleSelect(firstInput);
+
+const secondInput = document.querySelector("#second_input");
+handleSelect(secondInput);
+
+/* Cropper */
 
 const image = document.getElementById('popup__avatar');
 const cropper = new Cropper(image, {
